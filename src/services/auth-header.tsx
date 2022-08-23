a514361516cad914487b3d507e74e8723d60f6af
Ana Muraya <Anamuraya9@gmail.com>
2022-08-23 23:19:21 +0300

    Added authHeader to return authorization header

--- src/services/auth-header.tsx
+++ src/services/auth-header.tsx
@@ -1,1 +1,11 @@
-export {}
+const authHeader = () => {
+  const userStr = localStorage.getItem('user')
+  let user = null
+  if (userStr) user = JSON.parse(userStr)
+  if (user && user.accessToken) {
+    return { Authorization: 'Bearer ' + user.accessToken }
+  } else {
+    return { Authorization: '' }
+  }
+}
+export default authHeader()
