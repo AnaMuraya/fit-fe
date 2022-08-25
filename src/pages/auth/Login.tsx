b1c7d665e494fa19b70799c20b803812e78c88b6
Ana Muraya <Anamuraya9@gmail.com>
2022-08-25 05:35:11 +0300

    Added the login form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,26 @@
+import { useFormik } from 'formik'
+
 import styles from './style.module.scss'
 
 const Login = () => {
-  return <div className={styles.wrapper}>Login</div>
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      password: ''
+    },
+    onSubmit: function(values){
+      alert(`Name: ${values.name}, Password: ${values.password}`)
+    }
+  })
+  return <div className={styles.wrapper}>
+    <form onSubmit={formik.handleSubmit}>
+      <h3>Login</h3>
+      <div>
+        <label htmlFor="name">Username</label>
+        <input type="text" name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name}/>
+      </div>
+    </form>
+  </div>
 }
 
 export default Login
